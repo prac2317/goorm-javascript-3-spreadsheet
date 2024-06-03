70b3f43ad7b0a6a0f116fa47e13a2fc62e5b063e
prac2317 <lowdy92@naver.com>
2024-06-03 11:26:09 +0900

    과제 다시 만들기

--- script.js
+++ script.js
@@ -1,139 +1,55 @@
-const spreadSheetContainer = document.querySelector('#spreadsheet-container');
+const cellSheet = [];
+const alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'];
 
-const exportBtn = document.querySelector('#export-btn');
-
-const ROWS = 10;
-const COLS = 10;
-
-const spreadsheet = [];
-const alphabets = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
-];
-
-exportBtn.onclick = function (e) {
-  let csv = '';
-
-  for (let i = 0; i < spreadsheet.length; i++) {
-    if (i === 0) continue;
-    csv +=
-      spreadsheet[i]
-        .filter((item) => !item.isHeader)
-        .map((item) => item.data)
-        .join(',') + '\r\n';
-  }
-
-  const csvObj = new Blob([csv]);
-  console.log('csvObj', csvObj);
-
-  const csvUrl = URL.createObjectURL(csvObj);
-  console.log('csvUrl', csvUrl);
-
-  const a = document.createElement('a');
-  a.href = csvUrl;
-  a.download = 'spreadsheet name.csv';
-  a.click();
-};
-
-class Cell {
-  constructor(
-    isHeader,
-    disabled,
-    data,
+//---cell 만들기---//
+function createCell(row, column, cellRow) {
+  const cell = {
     row,
     column,
-    rowName,
-    columnName,
-    active = false
-  ) {
-    this.isHeader = isHeader;
-    this.disabled = disabled;
-    this.data = data;
-    this.row = row;
-    this.column = column;
-    this.rowName = rowName;
-    this.columnName = columnName;
-    this.active = active;
+    content: '',
+    isClicked: false,
+    isHeader: false,
+    isActive: false,
+  };
+
+  // header에 추가 속성 부여
+  if (row === 0 && column >= 1) {
+    cell.isHeader = true;
+    cell.content = alphabets[column - 1];
+  } else if (row >= 1 && column === 0) {
+    cell.isHeader = true;
+    cell.content = row;
+  } else if (row === 0 && column === 0) {
+    cell.isHeader = true;
+  }
+
+  cellRow.push(cell);
+}
+
+function createCellRow(row) {
+  const cellRow = [];
+  for (let i = 0; i < 10; i++) {
+    createCell(row, i, cellRow);
+  }
+  cellSheet.push(cellRow);
+}
+
+function createCellSheet() {
+  for (let i = 0; i < 10; i++) {
+    createCellRow(i);
   }
 }
 
-initSpreadsheet();
+createCellSheet();
 
-function initSpreadsheet() {
-  for (let i = 0; i < ROWS; i++) {
-    let spreadsheetRow = [];
-    for (let j = 0; j < COLS; j++) {
-      let cellData = '';
-      let isHeader = false;
-      let disabled = false;
-
-      if (j === 0) {
-        cellData = i;
-        isHeader = true;
-        disabled = true;
-      }
-
-      if (i === 0) {
-        cellData = alphabets[j - 1];
-        isHeader = true;
-        disabled = true;
-      }
-
-      if (!cellData) {
-        cellData = '';
-      }
-
-      const rowName = i;
-      const columnName = alphabets[j - 1];
-
-      const cell = new Cell(
-        isHeader,
-        disabled,
-        cellData,
-        i,
-        j,
-        rowName,
-        columnName,
-        false
-      );
-      spreadsheetRow.push(cell);
-    }
-    spreadsheet.push(spreadsheetRow);
-  }
-  drawSheet();
-}
+//---만들어진 cell 바탕으로 요소 만들기---//
+const cellContainer = document.getElementById('cell-container');
 
 function createCellEl(cell) {
   const cellEl = document.createElement('input');
   cellEl.className = 'cell';
-  cellEl.id = 'cell_' + cell.row + cell.column;
-  cellEl.value = cell.data;
-  cellEl.disabled = cell.disabled;
-
+  cellEl.id = `cell-${cell.row}-${cell.column}`;
+  cellEl.value = cell.content;
   if (cell.isHeader) {
     cellEl.classList.add('header');
   }
@@ -144,20 +60,38 @@ function createCellEl(cell) {
   return cellEl;
 }
 
-function handleOnChange(data, cell) {
-  cell.data = data;
+function handleOnChange(content, cell) {
+  cell.content = content;
 }
 
+function drawSheet() {
+  for (let i = 0; i < 10; i++) {
+    const cellRowEl = document.createElement('div');
+    cellRowEl.className = 'cell-row';
+
+    for (let j = 0; j < 10; j++) {
+      const cell = cellSheet[i][j];
+      cellRowEl.append(createCellEl(cell));
+    }
+
+    cellContainer.append(cellRowEl);
+  }
+}
+
+drawSheet();
+
+//---클릭하면 헤더 색 변하기---//
 function handleCellClick(cell) {
   clearHeaderActiveStates();
-  const columnHeader = spreadsheet[0][cell.column];
-  const rowHeader = spreadsheet[cell.row][0];
+  const columnHeader = cellSheet[0][cell.column];
+  const rowHeader = cellSheet[cell.row][0];
+  console.log(columnHeader, rowHeader);
   const columnHeaderEl = getElFromRowCol(columnHeader.row, columnHeader.column);
   const rowHeaderEl = getElFromRowCol(rowHeader.row, rowHeader.column);
   columnHeaderEl.classList.add('active');
   rowHeaderEl.classList.add('active');
   document.querySelector('#cell-status').innerHTML =
-    cell.columnName + cell.rowName;
+    alphabets[cell.column - 1] + cell.row;
 }
 
 function clearHeaderActiveStates() {
@@ -169,18 +103,33 @@ function clearHeaderActiveStates() {
 }
 
 function getElFromRowCol(row, col) {
-  return document.querySelector('#cell_' + row + col);
+  return document.querySelector(`#cell-${row}-${col}`);
 }
 
-function drawSheet() {
-  for (let i = 0; i < spreadsheet.length; i++) {
-    const rowContainerEl = document.createElement('div');
-    rowContainerEl.className = 'cell-row';
+//---excel 파일로 생성하기---//
+const exportBtn = document.querySelector('#export-btn');
 
-    for (let j = 0; j < spreadsheet[i].length; j++) {
-      const cell = spreadsheet[i][j];
-      rowContainerEl.append(createCellEl(cell));
-    }
-    spreadSheetContainer.append(rowContainerEl);
+exportBtn.onclick = function (e) {
+  let csv = '';
+  console.log(cellSheet);
+
+  for (let i = 0; i < cellSheet.length; i++) {
+    if (i === 0) continue;
+    csv +=
+      cellSheet[i]
+        .filter((item) => !item.isHeader)
+        .map((item) => item.content)
+        .join(',') + '\r\n';
   }
-}
+
+  const csvObj = new Blob([csv]);
+  console.log('csvObj', csvObj);
+
+  const csvUrl = URL.createObjectURL(csvObj);
+  console.log('csvUrl', csvUrl);
+
+  const a = document.createElement('a');
+  a.href = csvUrl;
+  a.download = 'spreadsheet.csv';
+  a.click();
+};
